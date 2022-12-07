fdef7c9b4165cd9af4bd4d970b44435acecc610d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-07 22:43:34 +0530

    added: layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
-import { Inter } from "@next/font/google";
 import Navbar from "./Navbar";
+import SideBar from "./Sidebar";
+import { Inter } from "@next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -8,9 +9,16 @@ const inter = Inter({
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`${inter.variable} font-sans`}>
+    <div
+      className={`min-h-screen border border-transparent bg-gray-800 ${inter.variable} font-sans`}
+    >
       <Navbar />
-      {children}
+      <main className=" mt-16 grid min-h-full grid-cols-12 bg-gray-800 bg-opacity-95">
+        <div className="col-span-2 border border-gray-700 bg-[#1F2937] sm:hidden lg:block">
+          <SideBar />
+        </div>
+        {children}
+      </main>
     </div>
   );
 };
