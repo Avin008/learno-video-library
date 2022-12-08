7e1d5349d7373be30c583a1dc500690c308f93d8
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-08 23:02:56 +0530

    added: responsive suggested card text

--- components/SuggestedVideoCard.tsx
+++ components/SuggestedVideoCard.tsx
@@ -21,9 +21,14 @@ const SuggestedVideoCard = ({
         <Image src={videoData.thumbnail} alt="" fill />
       </div>
       <div className="col-span-4 space-y-1">
-        <h1 className="text-sm font-semibold leading-5 text-gray-300">
+        <h1 className="text-sm font-semibold leading-5 text-gray-300 sm:hidden md:block">
           {videoData.title.length > 50
             ? `${videoData.title.slice(0, 50)}...`
+            : videoData.title}
+        </h1>
+        <h1 className="text-sm font-semibold leading-5 text-gray-300 sm:block md:hidden">
+          {videoData.title.length > 30
+            ? `${videoData.title.slice(0, 30)}...`
             : videoData.title}
         </h1>
         <h2 className="text-sm font-semibold text-gray-400">
