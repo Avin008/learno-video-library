122e2278215e38ab88907cf5b16040d18f26967b
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-08 22:56:34 +0530

    added: suggested video card

--- pages/[videoID].tsx
+++ pages/[videoID].tsx
@@ -1,22 +1,29 @@
-import Image from "next/image";
 import { data } from ".";
 import SuggestedVideoCard from "../components/SuggestedVideoCard";
 
 const SingleVideoPage = (): React.ReactElement => {
   return (
     <div className="mt-2 grid gap-5 px-2 sm:col-span-12 sm:grid-cols-1 md:grid-cols-8 lg:col-span-10">
-      <div className="relative aspect-video border border-gray-200 md:col-span-5">
-        <iframe
-          height="100%"
-          width="100%"
-          src="https://www.youtube.com/embed/RbOOwTcXDPs"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
+      <div className="md:col-span-5">
+        <div className="relative aspect-video">
+          <iframe
+            height="100%"
+            width="100%"
+            src="https://www.youtube.com/embed/RbOOwTcXDPs"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        </div>
+        <div className="mt-5 px-1">
+          <h1 className="text-xl font-medium text-gray-300">
+            Yuval Noah Harari | 21 Lessons for the 21st Century | Talks at
+            Google
+          </h1>
+        </div>
       </div>
       <div className="space-y-5 p-1 md:col-span-3">
         {data.map((x) => (
-          <SuggestedVideoCard videoData={x} />
+          <SuggestedVideoCard key={x._id} videoData={x} />
         ))}
       </div>
     </div>
