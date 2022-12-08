971bc9af9c25600e56716a877768ca381bdc99ea
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-08 23:47:56 +0530

    added: active link style && pages

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import {
   MdFavorite,
   MdHistory,
@@ -7,21 +8,56 @@ import {
 } from "react-icons/md";
 
 const SideBar = (): React.ReactElement => {
+  const router = useRouter();
+
+  const navigate = (route: string) => {
+    router.push(`/${route}`);
+  };
+
+  const activeLink = (route: string) => {
+    return router.asPath === route;
+  };
+
   return (
     <ul className="sticky top-16 flex flex-col space-y-3">
-      <li className="flex items-center gap-3 bg-hover p-4 text-white hover:cursor-pointer hover:bg-hover">
+      <li
+        className={`flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover ${
+          activeLink("/") && "bg-hover"
+        }`}
+        onClick={() => navigate("/")}
+      >
         <MdHome size={20} /> HOME
       </li>
-      <li className=" flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover">
+      <li
+        className={`flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover ${
+          activeLink("/playlist") && "bg-hover"
+        }`}
+        onClick={() => navigate("playlist")}
+      >
         <MdOutlineFeaturedPlayList size={20} /> PLAYLIST
       </li>
-      <li className=" flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover">
+      <li
+        className={`flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover ${
+          activeLink("/liked") && "bg-hover"
+        }`}
+        onClick={() => navigate("liked")}
+      >
         <MdFavorite size={20} /> LIKED
       </li>
-      <li className=" flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover">
+      <li
+        className={`flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover ${
+          activeLink("/watch-later") && "bg-hover"
+        }`}
+        onClick={() => navigate("watch-later")}
+      >
         <MdWatchLater size={20} /> WATCH LATER
       </li>
-      <li className=" flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover">
+      <li
+        className={`flex items-center gap-3 p-4 text-white hover:cursor-pointer hover:bg-hover ${
+          activeLink("/history") && "bg-hover"
+        }`}
+        onClick={() => navigate("history")}
+      >
         <MdHistory size={20} /> HISTORY
       </li>
     </ul>
---
+++ pages/playlist/index.tsx
@@ -0,0 +1,7 @@
+const PlaylistPage = (): React.ReactElement => {
+  return (
+    <div className="grid gap-5 p-2 sm:col-span-12 sm:grid-cols-1 md:grid-cols-2 lg:col-span-10 lg:grid-cols-3"></div>
+  );
+};
+
+export default PlaylistPage;
