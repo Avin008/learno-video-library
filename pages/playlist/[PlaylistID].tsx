f1b92f2cca9ab2a611e646910eeb11c753548ce6
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-09 12:18:50 +0530

    added: playlist video card

--- pages/playlist/[PlaylistID].tsx
+++ pages/playlist/[PlaylistID].tsx
@@ -1,7 +1,14 @@
+import { data } from "..";
+import PlaylistVideoCard from "../../components/PlaylistVideoCard";
+
 const PlaylistPage = (): React.ReactElement => {
   return (
     <div className="sm:col-span-12 lg:col-span-10">
-      <h1>Hello world</h1>
+      <div className="grid gap-5 p-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {data.map((x) => (
+          <PlaylistVideoCard data={x} />
+        ))}
+      </div>
     </div>
   );
 };
