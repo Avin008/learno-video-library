913a2cc44330f32e3a17dfded5821871d58595ab
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-09 12:36:14 +0530

    added: liked video card

--- pages/liked/index.tsx
+++ pages/liked/index.tsx
@@ -1,6 +1,13 @@
+import { data } from "..";
+import LikedVideoCard from "../../components/LikedVideoCard";
+
 const LikedPage = (): React.ReactElement => {
   return (
-    <div className="grid gap-5 p-2 sm:col-span-12 sm:grid-cols-1 md:grid-cols-2 lg:col-span-10 lg:grid-cols-3"></div>
+    <div className="grid gap-5 p-2 sm:col-span-12 sm:grid-cols-1 md:grid-cols-2 lg:col-span-10 lg:grid-cols-3">
+      {data.map((x) => (
+        <LikedVideoCard data={x} />
+      ))}
+    </div>
   );
 };
 
