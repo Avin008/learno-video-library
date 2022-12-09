f54c1491519f5219aea08634e67e45fdc8bb4752
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-09 11:57:49 +0530

    added: playlist video page

---
+++ pages/playlist/[PlaylistID].tsx
@@ -0,0 +1,9 @@
+const PlaylistPage = (): React.ReactElement => {
+  return (
+    <div className="sm:col-span-12 lg:col-span-10">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default PlaylistPage;
