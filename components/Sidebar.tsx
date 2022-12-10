a387ca7583484c8cac7678da62c2c04524bcbff8
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 22:40:27 +0530

    style: bg dark theme

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,40 +24,41 @@ const SideBar = (): React.ReactElement => {
   return (
     <ul className="sticky top-16 flex flex-col space-y-3">
       <li
-        className={`flex items-center gap-3 p-4 text-dark-text hover:cursor-pointer hover:bg-dark-hover ${
-          activeLink("/") && "bg-dark-hover"
+        className={`flex items-center gap-3 p-4 hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
+          activeLink("/") && "dark:bg-dark-hover"
         }`}
         onClick={() => navigate("/")}
       >
         <MdHome size={20} /> HOME
       </li>
       <li
-        className={`flex items-center gap-3 p-4 text-dark-text hover:cursor-pointer hover:bg-dark-hover ${
-          activeLink("/playlist", "/playlist/[PlaylistID]") && "bg-dark-hover"
+        className={`flex items-center gap-3 p-4 hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
+          activeLink("/playlist", "/playlist/[PlaylistID]") &&
+          "dark:bg-dark-hover"
         }`}
         onClick={() => navigate("playlist")}
       >
         <MdOutlineFeaturedPlayList size={20} /> PLAYLIST
       </li>
       <li
-        className={`flex items-center gap-3 p-4 text-dark-text hover:cursor-pointer hover:bg-dark-hover ${
-          activeLink("/liked") && "bg-dark-hover"
+        className={`flex items-center gap-3 p-4 hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
+          activeLink("/liked") && "dark:bg-dark-hover"
         }`}
         onClick={() => navigate("liked")}
       >
         <MdFavorite size={20} /> LIKED
       </li>
       <li
-        className={`flex items-center gap-3 p-4 text-dark-text hover:cursor-pointer hover:bg-dark-hover ${
-          activeLink("/watch-later") && "bg-dark-hover"
+        className={`flex items-center gap-3 p-4 hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
+          activeLink("/watch-later") && "dark:bg-dark-hover"
         }`}
         onClick={() => navigate("watch-later")}
       >
         <MdWatchLater size={20} /> WATCH LATER
       </li>
       <li
-        className={`flex items-center gap-3 p-4 text-dark-text hover:cursor-pointer hover:bg-dark-hover ${
-          activeLink("/history") && "bg-dark-hover"
+        className={`flex items-center gap-3 p-4 hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
+          activeLink("/history") && "dark:bg-dark-hover"
         }`}
         onClick={() => navigate("history")}
       >
