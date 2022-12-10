85fde04290d5541544acc30c92c85355b399e074
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 23:52:24 +0530

    dark mode style

--- components/SingleVideoCard.tsx
+++ components/SingleVideoCard.tsx
@@ -23,11 +23,11 @@ const SingleVideoCard = ({
         ></iframe>
       </div>
       <div className="px-2 sm:space-y-1 md:space-y-4 lg:space-y-2">
-        <h1 className="text-lg font-medium text-gray-300">
+        <h1 className="text-lg font-medium dark:text-gray-300">
           Yuval Noah Harari | 21 Lessons for the 21st Century | Talks at Google
         </h1>
         <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3 font-semibold text-gray-400">
+          <div className="flex items-center gap-3 font-semibold dark:text-gray-400">
             <div className="relative h-10 w-10 rounded-full">
               <Image
                 className="rounded-full"
@@ -39,19 +39,19 @@ const SingleVideoCard = ({
             <span>Ted Talks</span>
           </div>
         </div>
-        <div className="flex flex-wrap gap-5 text-xs text-gray-100 sm:justify-end">
-          <span className="flex w-fit cursor-pointer items-center gap-2 rounded-md border  border-primary p-1 hover:bg-hover lg:px-4">
+        <div className="flex flex-wrap gap-5 text-xs dark:text-gray-100 sm:justify-end">
+          <span className="flex w-fit cursor-pointer items-center gap-2 rounded-md border p-1 dark:hover:bg-dark-hover lg:px-4">
             <MdOutlineThumbUp size={18} />
             <span className="sm:hidden md:block">LIKE</span>
           </span>
           <span
-            className="flex w-fit cursor-pointer items-center gap-2 rounded-md border border-primary  p-1 hover:bg-hover lg:px-4"
+            className="border-primary flex w-fit cursor-pointer items-center gap-2 rounded-md border  p-1 dark:hover:bg-dark-hover lg:px-4"
             onClick={togglePlaylistModal}
           >
             <MdPlaylistAdd size={18} />
             <span className="sm:hidden md:block">ADD TO PLAYLIST</span>
           </span>
-          <span className="flex w-fit cursor-pointer items-center gap-2 rounded-md border border-primary  p-1 hover:bg-hover lg:px-4">
+          <span className="border-primary dark:hover:bg-hover flex w-fit cursor-pointer items-center gap-2 rounded-md  border p-1 lg:px-4">
             <MdWatchLater size={18} />
             <span className="sm:hidden md:block">WATCH LATER</span>
           </span>
