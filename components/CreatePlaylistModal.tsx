dcb218cdecf2fad07c84fb73e399f89bfcbdfe00
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 00:05:23 +0530

    dark mode style

--- components/CreatePlaylistModal.tsx
+++ components/CreatePlaylistModal.tsx
@@ -16,28 +16,30 @@ const CreatePlaylistModal = ({
 
   return (
     <div
-      className="fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-black bg-opacity-30 transition-all"
+      className="fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-black bg-opacity-30"
       onClick={togglePlaylistModal}
     >
       <div
-        className="mx-2 h-fit w-80 rounded-md bg-gray-800 shadow-md"
+        className="mx-2 h-fit w-80 rounded-md shadow-md dark:bg-dark-background"
         onClick={handlePlaylistModalClose}
       >
-        <div className="flex items-center justify-between border-b border-gray-700 p-2">
-          <h1 className="text-lg font-normal text-gray-200">Save to...</h1>
+        <div className="flex items-center justify-between border-b p-2 dark:border-dark-border">
+          <h1 className="text-lg font-normal dark:text-dark-text">
+            Save to...
+          </h1>
           <MdClose
-            className="text-2xl text-gray-200 hover:cursor-pointer"
+            className="text-2xl hover:cursor-pointer dark:text-dark-text"
             onClick={togglePlaylistModal}
           />
         </div>
-        <ul className="h-40 space-y-3 overflow-y-scroll py-1 text-gray-200 shadow-inner">
+        <ul className="h-40 space-y-3 overflow-y-scroll py-1 shadow-inner dark:text-dark-text">
           {data.map((x) => (
             <li
               className="flex items-center gap-3 p-2 hover:cursor-pointer"
               key={x}
             >
               <input
-                className="h-4 w-4 border-2 border-gray-200"
+                className="h-4 w-4 border-2 dark:border-dark-primary"
                 type="checkbox"
                 name=""
                 id=""
@@ -46,10 +48,9 @@ const CreatePlaylistModal = ({
             </li>
           ))}
         </ul>
-        <div className="flex h-fit flex-col space-y-3 border-t border-gray-700 px-3 pb-3">
-          <label className="text-gray-200" htmlFor=""></label>
+        <div className="flex h-fit flex-col space-y-3 border-t px-3 pb-3 dark:border-gray-700">
           <input
-            className="w-full border-b border-primary bg-transparent p-1 text-gray-300 outline-none placeholder:text-gray-500"
+            className="w-full border-b bg-transparent p-1 outline-none placeholder:text-gray-500 dark:border-dark-primary dark:text-dark-text"
             type="text"
             name=""
             id=""
@@ -64,7 +65,7 @@ const CreatePlaylistModal = ({
           <div className="w-full">
             {" "}
             <button
-              className="w-full rounded-md bg-primary p-1 text-gray-100"
+              className="w-full rounded-md p-1 dark:bg-dark-primary dark:text-dark-text"
               onClick={() => setShowError((prev) => !prev)}
             >
               Create new playlist
