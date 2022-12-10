84c11b883946b7b3284dde9585e65ef09b9d0fbc
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 23:33:55 +0530

    dark mode style

--- components/PlaylistCardMenu.tsx
+++ components/PlaylistCardMenu.tsx
@@ -7,9 +7,9 @@ const PlaylistCardMenu = (): React.ReactElement => {
   };
 
   return (
-    <ul className="bg-background absolute bottom-1 right-2 z-10 h-fit w-fit space-y-1 border border-dark-border bg-dark-background py-1 text-xs text-gray-200 shadow-md transition-all">
+    <ul className="absolute bottom-1 right-2 z-10 h-fit w-fit space-y-1 border py-1 text-xs shadow-md transition-all dark:border-dark-border dark:bg-dark-background dark:text-gray-200">
       <li
-        className="flex items-center gap-2 p-2 transition-all hover:bg-dark-hover"
+        className="flex items-center gap-2 p-2 transition-all dark:hover:bg-dark-hover"
         onClick={handleClick}
       >
         <MdPlaylistAdd className="text-xl" /> REMOVE FROM PLAYLIST
