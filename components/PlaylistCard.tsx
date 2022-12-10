9d75b2426b97bf770f6197ed4d81acb703ea7480
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 23:30:04 +0530

    dark mode style

--- components/PlaylistCard.tsx
+++ components/PlaylistCard.tsx
@@ -41,22 +41,22 @@ const PlaylistCard = ({ data }: { data: Props }): React.ReactElement => {
           onClick={navigate}
         />
         {showVideoOptions && <PlaylistCardMenu />}
-        <div className="absolute top-0 bottom-0 left-[50%] right-0 flex items-center justify-center bg-gray-800 bg-opacity-70">
-          <span className="text-3xl font-semibold text-white">5</span>
+        <div className="absolute top-0 bottom-0 left-[50%] right-0 flex items-center justify-center bg-opacity-70 dark:bg-gray-800">
+          <span className="text-3xl font-semibold dark:text-white">5</span>
         </div>
       </div>
       <div className="grid grid-cols-8 pt-3">
         <div className="col-span-6 space-y-1">
-          <h2 className="text-base font-semibold text-gray-300">
+          <h2 className="text-base font-semibold dark:text-gray-300">
             {data.channelName}
           </h2>
         </div>
         <div className="col-span-2 flex items-start justify-end">
           <span
-            className="rounded-full p-1 hover:cursor-pointer hover:bg-dark-hover"
+            className="rounded-full p-1 hover:cursor-pointer dark:hover:bg-dark-hover"
             onClick={toggleVideoOptions}
           >
-            <MdMoreVert className="rounded-full text-gray-200" size={25} />
+            <MdMoreVert className="rounded-full dark:text-gray-200" size={25} />
           </span>
         </div>
       </div>
