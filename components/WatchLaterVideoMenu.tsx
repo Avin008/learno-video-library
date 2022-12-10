d58ec5c3a9fae44892320acc60835181a42c3fe9
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 19:18:23 +0530

    style: added video options card border,hover,bg color

--- components/WatchLaterVideoMenu.tsx
+++ components/WatchLaterVideoMenu.tsx
@@ -7,9 +7,9 @@ const WatchLaterVideoCardMenu = (): React.ReactElement => {
   };
 
   return (
-    <ul className="absolute bottom-1 right-2 z-10 h-fit w-fit space-y-1 border border-gray-600 bg-background bg-opacity-95 py-1 text-xs text-gray-200 shadow-md transition-all">
+    <ul className="bg-background absolute bottom-1 right-2 z-10 h-fit w-fit space-y-1 border border-dark-border bg-dark-background py-1 text-xs text-gray-200 shadow-md transition-all">
       <li
-        className="flex items-center gap-2 p-2 transition-all hover:bg-hover"
+        className="flex items-center gap-2 p-2 transition-all hover:bg-dark-hover"
         onClick={handleClick}
       >
         <MdPlaylistAdd className="text-xl" /> REMOVE FROM WATCH LATER
