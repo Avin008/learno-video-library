61e114c6cfc1043efca7be16e95cf0a1a2cb40ae
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 22:22:52 +0530

    style: bg dark theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,12 +15,12 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`${darkMode && "dark"}`}>
       <div
-        className={` min-h-screen border border-transparent bg-dark-background ${inter.variable} font-sans`}
+        className={` min-h-screen border border-transparent dark:bg-dark-background ${inter.variable} font-sans`}
       >
         <Navbar />
         <main className=" mt-16 grid min-h-full grid-cols-12 sm:relative lg:static">
           <div
-            className={`col-span-2 border-r border-dark-border transition-all sm:absolute sm:bottom-0 sm:top-0 sm:z-10 lg:static ${
+            className={`col-span-2 border-r transition-all dark:border-dark-border sm:absolute sm:bottom-0 sm:top-0 sm:z-10 lg:static ${
               sidebar ? "sm:block" : "sm:hidden"
             } lg:block`}
           >
