efc499da015d88bc0f908b26cfa30cda0c9138b7
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-10 18:55:11 +0530

    style: added dark theme hover color

--- components/VideoCardMenu.tsx
+++ components/VideoCardMenu.tsx
@@ -7,16 +7,16 @@ const VideoCardMenu = ({
 }): React.ReactElement => {
   return (
     <ul className="bg-background absolute bottom-1 right-2 h-fit w-44 space-y-1 border border-dark-border bg-dark-background py-1 text-xs text-gray-200 shadow-md transition-all">
-      <li className="hover:bg-hover flex items-center gap-2 p-2 transition-all">
+      <li className="flex items-center gap-2 p-2 transition-all hover:bg-dark-hover">
         <MdThumbUp /> ADD TO LIKE
       </li>
       <li
-        className="hover:bg-hover flex items-center gap-2 p-2 transition-all"
+        className="flex items-center gap-2 p-2 transition-all hover:bg-dark-hover"
         onClick={togglePlaylistModal}
       >
         <MdPlaylistAdd /> ADD TO PLAYLIST
       </li>
-      <li className="hover:bg-hover flex items-center gap-2 p-2 transition-all">
+      <li className="flex items-center gap-2 p-2 transition-all hover:bg-dark-hover">
         <MdWatchLater /> WATCH LATER
       </li>
     </ul>
