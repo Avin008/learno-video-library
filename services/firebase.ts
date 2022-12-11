73ec4e8bf2c07222225068468bd7297d6b3bde8d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 15:36:17 +0530

    added: env variables

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmTdJIq6elVt4zuWyVVV0hiAg8Oh4kS-I",
-  authDomain: "video-library-app-3e865.firebaseapp.com",
-  projectId: "video-library-app-3e865",
-  storageBucket: "video-library-app-3e865.appspot.com",
-  messagingSenderId: "891743970057",
-  appId: "1:891743970057:web:ec73eb49462565ecf4e9d8",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
