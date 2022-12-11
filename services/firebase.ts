cdd5874dea1c3ba581bdbc350c856c23a9f63d92
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 15:20:05 +0530

    added: firebase config file

---
+++ services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmTdJIq6elVt4zuWyVVV0hiAg8Oh4kS-I",
+  authDomain: "video-library-app-3e865.firebaseapp.com",
+  projectId: "video-library-app-3e865",
+  storageBucket: "video-library-app-3e865.appspot.com",
+  messagingSenderId: "891743970057",
+  appId: "1:891743970057:web:ec73eb49462565ecf4e9d8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
