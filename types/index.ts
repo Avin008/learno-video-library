4a4ff469fa9bd356a5dae0b29dd3e8f9ada53d41
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 16:01:40 +0530

    added: types

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type Videos = {
+  id: string;
+  categoryId: number;
+  title: string;
+  thumbnail: string;
+  channelIcon: string;
+  channelName: string;
+  videoLink: string;
+  category: string;
+};
