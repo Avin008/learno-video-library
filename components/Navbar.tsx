75885f9b46596f01e5dcef383fdc2444b6d8d504
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 15:06:44 +0530

    added: toggle login && logout btn && loading state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { MdOutlineDarkMode, MdLightMode, MdOutlineMenu } from "react-icons/md";
-import { useSidebarStore, useThemeStore } from "../store";
+import { useAuth } from "../hooks";
+import { useSidebarStore, useThemeStore, useAuthStore } from "../store";
+import { ClipLoader } from "react-spinners";
 
 const Navbar = (): React.ReactElement => {
   const expandSidebar = useSidebarStore((store) => store.expandSidebar);
   const darkMode = useThemeStore((store) => store.darkMode);
   const toggleDarkMode = useThemeStore((store) => store.toggleDarkMode);
+  const addAuth = useAuthStore((store: any) => store.addAuth);
+  const removeAuth = useAuthStore((store: any) => store.removeAuth);
+
+  const { status, loading, token } = useAuth();
 
   const router = useRouter();
 
@@ -30,15 +36,25 @@ const Navbar = (): React.ReactElement => {
         className="flex items-center
        gap-6 text-base font-medium transition-all"
       >
-        <Link href="/login">
+        {loading ? (
+          <ClipLoader color="gray" size={25} />
+        ) : status ? (
           <li
-            className={`rounded-md bg-dark-primary px-3 py-2 text-sm font-medium text-white hover:cursor-pointer ${
-              router.pathname === "/login" && ""
-            }`}
+            className={`rounded-md bg-dark-primary px-3 py-2 text-sm font-medium text-white hover:cursor-pointer`}
           >
-            LOGIN
+            LOGOUT
           </li>
-        </Link>
+        ) : (
+          <Link href="/login">
+            <li
+              className={`rounded-md bg-dark-primary px-3 py-2 text-sm font-medium text-white hover:cursor-pointer ${
+                router.pathname === "/login" && ""
+              }`}
+            >
+              LOGIN
+            </li>
+          </Link>
+        )}
         <li
           className="rounded-md px-3 py-2 text-sm font-medium transition-all  hover:cursor-pointer dark:text-gray-300 dark:hover:bg-dark-hover dark:hover:text-white sm:fixed sm:top-3 sm:right-5 md:static"
           onClick={toggleDarkMode}
