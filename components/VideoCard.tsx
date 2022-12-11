e6f204ed18462d2b503af237bd476fe9300d93b8
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-11 16:55:07 +0530

    added: video data

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -3,22 +3,13 @@ import { MdMoreVert } from "react-icons/md";
 import VideoCardMenu from "./VideoCardMenu";
 import { useState } from "react";
 import { useRouter } from "next/router";
-
-type Props = {
-  _id: string;
-  title: string;
-  thumbnail: string;
-  channelIcon: string;
-  channelName: string;
-  videoLink: string;
-  category: string;
-};
+import { Videos } from "../types";
 
 const VideoCard = ({
-  data,
+  videoData,
   togglePlaylistModal,
 }: {
-  data: Props;
+  videoData: Videos;
   togglePlaylistModal: () => void;
 }): React.ReactElement => {
   const [showVideoOptions, setShowVideoOptions] = useState<boolean>(false);
@@ -30,7 +21,7 @@ const VideoCard = ({
   const router = useRouter();
 
   const navigate = () => {
-    router.push(`/video/${data._id}`);
+    router.push(`/video/${videoData.id}`);
   };
 
   return (
@@ -38,7 +29,7 @@ const VideoCard = ({
       <div className="relative aspect-video hover:cursor-pointer">
         <Image
           className=""
-          src={data.thumbnail}
+          src={videoData.thumbnail}
           alt=""
           fill
           onClick={navigate}
@@ -52,7 +43,7 @@ const VideoCard = ({
           <div className="relative h-10 w-10">
             <Image
               className="rounded-full"
-              src={data.channelIcon}
+              src={videoData.channelIcon}
               alt=""
               fill
             />
@@ -60,12 +51,12 @@ const VideoCard = ({
         </div>
         <div className="col-span-5 space-y-1">
           <h1 className="text-sm font-semibold leading-5 dark:text-gray-300">
-            {data.title.length > 50
-              ? `${data.title.slice(0, 50)}..`
-              : data.title}
+            {videoData.title.length > 50
+              ? `${videoData.title.slice(0, 50)}..`
+              : videoData.title}
           </h1>
           <h2 className="text-sm font-semibold dark:text-gray-400">
-            {data.channelName}
+            {videoData.channelName}
           </h2>
         </div>
         <div className="col-span-1 flex items-start justify-end">
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
 import useAuth from "./useAuth";
+import useGetCollectionData from "./useGetCollectionData";
 
-export { useAuth };
+export { useAuth, useGetCollectionData };
