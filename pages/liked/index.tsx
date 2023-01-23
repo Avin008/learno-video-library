d5dc55acc70c2cd4c3fbd7b654aaa8e9c9787922
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-23 10:20:13 +0530

    refactor: removed unused imports

--- pages/liked/index.tsx
+++ pages/liked/index.tsx
@@ -1,6 +1,5 @@
 import { LoadingSpinner } from "../../components";
 import LikedVideoCard from "../../components/LikedVideoCard";
-import { data } from "../../data";
 import { useGetUserData } from "../../hooks";
 import { Video } from "../../types";
 
@@ -14,14 +13,12 @@ const LikedPage = (): React.ReactElement => {
   return (
     <div className="grid gap-5 p-2 sm:col-span-12 sm:grid-cols-1 md:grid-cols-2 lg:col-span-10 lg:grid-cols-3">
       {isUserDataLoading && <LoadingSpinner />}
-      {userData?.liked?.map(
-        (videoData: Video) => (
-          <LikedVideoCard
-            key={videoData.id}
-            videoData={videoData}
-          />
-        )
-      )}
+      {userData?.liked?.map((videoData: Video) => (
+        <LikedVideoCard
+          key={videoData.id}
+          videoData={videoData}
+        />
+      ))}
     </div>
   );
 };
