c95ee5b3f7e3b2dfedd5f5a676c62505664cea4b
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-25 11:23:31 +0530

    style: added dark mode styles

--- components/CategoryChips.tsx
+++ components/CategoryChips.tsx
@@ -8,20 +8,18 @@ const CategoryChips = ({
   categorySetterFunc,
 }: {
   videoData: Video[];
-  categorySetterFunc: (
-    categoryName: string
-  ) => void;
+  categorySetterFunc: (categoryName: string) => void;
   category: string | null;
 }) => {
   return (
-    <div className="sticky top-14 z-10 flex h-fit items-center gap-4 bg-opacity-95 px-2 py-2 pt-4 dark:bg-dark-background sm:overflow-x-scroll md:overflow-auto">
+    <div className="sticky top-14 z-10 flex h-fit items-center gap-4 bg-white bg-opacity-95 px-2 py-2 pt-4 dark:bg-dark-background sm:overflow-x-scroll md:overflow-auto">
       {removeDuplicate(videoData).map((x) => (
         <span
           key={x}
           className={`max-w-fit rounded-md border py-1 px-3 transition-all hover:cursor-pointer ${
             category === x
               ? "bg-dark-primary text-white"
-              : "bg-dark-hover"
+              : "bg-gray-200 dark:bg-dark-hover"
           } dark:border-gray-600 dark:text-dark-text`}
           onClick={() => {
             categorySetterFunc(x);
