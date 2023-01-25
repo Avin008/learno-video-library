e63df37a9fdc3100e9f37320173dc213686c8df5
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-25 11:07:56 +0530

    feat: redirect to login page when unauthorized

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,9 +11,11 @@ import {
   MdFeaturedPlayList,
   MdOutlineHistory,
 } from "react-icons/md";
+import { useAuth } from "../hooks";
 
 const SideBar = (): React.ReactElement => {
   const router = useRouter();
+  const { status: authStatus } = useAuth();
 
   const navigate = (route: string) => {
     router.push(`/${route}`);
@@ -35,7 +37,9 @@ const SideBar = (): React.ReactElement => {
         className={`flex items-center gap-3 p-4 transition-all hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
           activeLink("/") && "dark:bg-dark-hover"
         }`}
-        onClick={() => navigate("/")}
+        onClick={() => {
+          navigate("/");
+        }}
       >
         {activeLink("/") ? (
           <MdHome size={20} />
@@ -51,7 +55,13 @@ const SideBar = (): React.ReactElement => {
             "/playlist/[PlaylistID]"
           ) && "dark:bg-dark-hover"
         }`}
-        onClick={() => navigate("playlist")}
+        onClick={() => {
+          if (authStatus) {
+            navigate("playlist");
+          } else {
+            navigate("login");
+          }
+        }}
       >
         {activeLink("/playlist") ? (
           <MdFeaturedPlayList size={20} />
@@ -64,7 +74,13 @@ const SideBar = (): React.ReactElement => {
         className={`flex items-center gap-3 p-4 transition-all hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
           activeLink("/liked") && "dark:bg-dark-hover"
         }`}
-        onClick={() => navigate("liked")}
+        onClick={() => {
+          if (authStatus) {
+            navigate("liked");
+          } else {
+            navigate("login");
+          }
+        }}
       >
         {activeLink("/liked") ? (
           <MdFavorite size={20} />
@@ -77,7 +93,13 @@ const SideBar = (): React.ReactElement => {
         className={`flex items-center gap-3 p-4 transition-all hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
           activeLink("/watch-later") && "dark:bg-dark-hover"
         }`}
-        onClick={() => navigate("watch-later")}
+        onClick={() => {
+          if (authStatus) {
+            navigate("watch-later");
+          } else {
+            navigate("login");
+          }
+        }}
       >
         {activeLink("/watch-later") ? (
           <MdWatchLater size={20} />
@@ -90,7 +112,13 @@ const SideBar = (): React.ReactElement => {
         className={`flex items-center gap-3 p-4 transition-all hover:cursor-pointer dark:text-dark-text dark:hover:bg-dark-hover ${
           activeLink("/history") && "dark:bg-dark-hover"
         }`}
-        onClick={() => navigate("history")}
+        onClick={() => {
+          if (authStatus) {
+            navigate("history");
+          } else {
+            navigate("login");
+          }
+        }}
       >
         {activeLink("/history") ? (
           <MdHistory size={20} />
